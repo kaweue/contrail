0531dd70e70092e755bb639e2cc443c7f64fb3d1
Kaweue <pawel.marchewka1@gmail.com>
2018-12-26 23:35:47 +0100

    Neutron plugin port read, update, delete

--- pkg/neutron/logic/port.go
+++ pkg/neutron/logic/port.go
@@ -2,14 +2,15 @@ package logic
 
 import (
 	"context"
-	"strings"
 
 	"github.com/Juniper/contrail/pkg/errutil"
 	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/models/basemodels"
 	"github.com/twinj/uuid"
 
 	"github.com/Juniper/contrail/pkg/services"
 	"github.com/Juniper/contrail/pkg/services/baseservices"
+	"github.com/gogo/protobuf/types"
 	"github.com/pkg/errors"
 )
 
@@ -42,41 +43,155 @@ func (port *Port) Create(ctx context.Context, rp RequestParameters) (Response, e
 	// create interface route table for the port if
 	// subnet has a host route for this port ip.
 
-	return port.vncToNeutron(ctx, rp, vn, vmi, iip)
+	return makePortResponse(vn, vmi, []*models.InstanceIP{iip})
 }
 
-func (port *Port) vncToNeutron(
-	ctx context.Context, rp RequestParameters,
-	vn *models.VirtualNetwork, vmi *models.VirtualMachineInterface, iip *models.InstanceIP,
-) (*PortResponse, error) {
-
-	portRes := &PortResponse{
-		ID:                  vmi.GetUUID(),
-		NetworkID:           vn.GetUUID(),
-		PortSecurityEnabled: vmi.GetPortSecurityEnabled(),
-		Status:              "DOWN",
+// Update handles port update request
+func (port *Port) Update(ctx context.Context, rp RequestParameters) (Response, error) {
+	vmi, vn, err := port.readVNCPort(ctx, rp, port.ID)
+	if err != nil {
+		return nil, errors.Wrapf(err, "couldn't read vnc resources %s", port.ID)
 	}
 
-	if len(vmi.DisplayName) != 0 {
-		portRes.Name = vmi.DisplayName
-	} else if len(vmi.GetFQName()) >= 1 {
-		portRes.Name = vmi.GetFQName()[len(vmi.GetFQName())-1]
+	fm := types.FieldMask{}
+
+	if port.Name != "" {
+		vmi.DisplayName = port.Name
+		basemodels.FieldMaskAppend(&fm, models.VirtualMachineInterfaceFieldDisplayName)
 	}
 
-	if vmi.GetParentType() != models.KindProject {
-		portRes.TenantID = vn.GetParentUUID()
-	} else {
-		portRes.TenantID = vmi.GetParentUUID()
+	if port.DeviceOwner != "network:router_interface" &&
+		port.DeviceOwner != "network:router_gateway" && len(port.DeviceID) != 0 {
+		if err := port.setVMInstance(ctx, rp, vmi); err != nil {
+			return nil, err
+		}
+		basemodels.FieldMaskAppend(&fm, models.VirtualMachineInterfaceFieldVirtualMachineRefs)
 	}
-	portRes.TenantID = strings.Replace(portRes.TenantID, "-", "", -1)
-	macAddresses := vmi.GetVirtualMachineInterfaceMacAddresses().GetMacAddress()
-	if len(macAddresses) != 0 {
-		portRes.MacAddress = macAddresses[0]
+	//TODO handle key value pairs
+	//TODO handle mac address change
+	//TODO port security enabled update
+	//TODO id perms update
+	//TODO allowed_address_pairs update
+	//TODO fixed_ips update
+
+	if _, err = rp.WriteService.UpdateVirtualMachineInterface(ctx, &services.UpdateVirtualMachineInterfaceRequest{
+		VirtualMachineInterface: vmi,
+		FieldMask:               fm,
+	}); err != nil {
+		return nil, errors.Wrapf(err, "couldn't update vmi %s", vmi.GetUUID())
 	}
 
-	//TODO: Fill bindings
+	return makePortResponse(vn, vmi, vmi.GetInstanceIPBackRefs())
+}
 
-	return portRes, nil
+// Delete handles port delete requests
+func (port *Port) Delete(ctx context.Context, rp RequestParameters, id string) (Response, error) {
+	vmi, _, err := port.readVNCPort(ctx, rp, id)
+	if err != nil {
+		return nil, errors.Wrapf(err, "couldn't read vnc resources %s", id)
+	}
+
+	if len(vmi.GetLogicalRouterBackRefs()) > 0 {
+		return nil, errors.Errorf("L3PortInUse device_owner = network:router_interface port_id = %s", id)
+	}
+
+	// release instance IP address
+	for _, iip := range vmi.GetInstanceIPBackRefs() {
+		// TODO handle shared ip case
+		if _, err := rp.WriteService.DeleteInstanceIP(ctx, &services.DeleteInstanceIPRequest{
+			ID: iip.GetUUID(),
+		}); err != nil {
+			// instance ip could be deleted by svc monitor if it is
+			// a shared ip. Ignore this error
+		}
+	}
+
+	//TODO disassociate any floating IP used by instance
+
+	if _, err := rp.WriteService.DeleteVirtualMachineInterface(ctx, &services.DeleteVirtualMachineInterfaceRequest{
+		ID: vmi.GetUUID(),
+	}); err != nil {
+		return nil, errors.Wrapf(err, "couldn't delete virtual machine interface %s", vmi.GetUUID())
+	}
+
+	if vmID := port.getAsssociatedVirtualMachineID(vmi); vmID != "" {
+		_, err = rp.WriteService.DeleteVirtualMachine(ctx, &services.DeleteVirtualMachineRequest{
+			ID: vmID,
+		})
+		// delete instance if this was the last port
+		if err != nil && !errutil.IsNotFound(err) && !errutil.IsConflict(err) {
+			return nil, errors.Wrapf(err, "failed to delete virtual machine %s", vmID)
+		}
+	}
+
+	//TODO delete any interface route table associated with the port to handle
+	// subnet host route Neutron extension, un-reference others
+
+	return &PortResponse{}, nil
+}
+
+// Read default implementation
+func (port *Port) Read(ctx context.Context, rp RequestParameters, id string) (Response, error) {
+	vmi, vn, err := port.readVNCPort(ctx, rp, id)
+	if err != nil {
+		return nil, errors.Wrapf(err, "couldn't read vnc resources %s", id)
+	}
+
+	return makePortResponse(vn, vmi, vmi.GetInstanceIPBackRefs())
+}
+
+// ReadAll logic
+func (port *Port) ReadAll(ctx context.Context, rp RequestParameters, filters Filters, fields Fields) (Response, error) {
+	// TODO implement ReadAll logic
+	return []PortResponse{}, nil
+}
+
+func (port *Port) getAsssociatedVirtualMachineID(vmi *models.VirtualMachineInterface) string {
+	if vmi.GetParentType() == models.KindVirtualMachine {
+		return vmi.GetParentUUID()
+	}
+
+	if len(vmi.GetVirtualMachineRefs()) > 0 {
+		return vmi.VirtualMachineRefs[0].GetUUID()
+	}
+
+	return ""
+}
+
+func (port *Port) getAssociatedVirtualNetwork(ctx context.Context, rp RequestParameters,
+	vmi *models.VirtualMachineInterface,
+) (*models.VirtualNetwork, error) {
+	vnRefs := vmi.GetVirtualNetworkRefs()
+	if len(vnRefs) == 0 {
+		return nil, nil
+	}
+	vnRes, err := rp.ReadService.GetVirtualNetwork(ctx, &services.GetVirtualNetworkRequest{
+		ID: vnRefs[0].GetUUID(),
+	})
+
+	if err != nil {
+		return nil, errors.Wrapf(err, "couldn't get virtual network %s", vnRefs[0].GetUUID())
+	}
+	return vnRes.GetVirtualNetwork(), nil
+}
+
+func (port *Port) readVNCPort(
+	ctx context.Context, rp RequestParameters, id string,
+) (*models.VirtualMachineInterface, *models.VirtualNetwork, error) {
+	vmiRes, err := rp.ReadService.GetVirtualMachineInterface(ctx, &services.GetVirtualMachineInterfaceRequest{
+		ID: id,
+	})
+
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "couldn't get virtual machine interface %s", id)
+	}
+
+	vmi := vmiRes.GetVirtualMachineInterface()
+	vn, err := port.getAssociatedVirtualNetwork(ctx, rp, vmi)
+	if err != nil {
+		return nil, nil, err
+	}
+	return vmi, vn, nil
 }
 
 func (port *Port) allocateIPAddress(
@@ -203,6 +318,7 @@ func (port *Port) setVMInstance(ctx context.Context, rp RequestParameters,
 
 	vmi.AddVirtualMachineRef(&models.VirtualMachineInterfaceVirtualMachineRef{
 		UUID: vm.GetUUID(),
+		To:   vm.GetFQName(),
 	})
 
 	return nil
@@ -292,6 +408,7 @@ func (port *Port) createVirtualMachineInterface(
 			Key:   "vnic_type",
 			Value: port.BindingVnicType,
 		})
+		vmi.VirtualMachineInterfaceBindings = kvps
 	}
 
 	if err := port.setPortSecurity(ctx, rp, vmi, vn); err != nil {
@@ -336,15 +453,4 @@ func (port *Port) checkMacAddress(ctx context.Context, rp RequestParameters) err
 	}
 
 	return nil
-}
-
-// Read default implementation
-func (port *Port) Read(ctx context.Context, rp RequestParameters, id string) (Response, error) {
-	return PortResponse{}, nil
-}
-
-// ReadAll logic
-func (port *Port) ReadAll(ctx context.Context, rp RequestParameters, filters Filters, fields Fields) (Response, error) {
-	// TODO implement ReadAll logic
-	return []PortResponse{}, nil
 }
